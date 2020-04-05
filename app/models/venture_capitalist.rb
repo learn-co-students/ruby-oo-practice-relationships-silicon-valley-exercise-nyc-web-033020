86f47fab4df8bf1962a3398e19ab6cfc4a4016fa
mishyjari <mishyjari@outlook.com>
2020-04-05 10:51:51 -0400

    all deliverables met

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -17,24 +17,26 @@ class VentureCapitalist
     VentureCapitalist.all.select{ |person| person.net_worth >= 1000000000 }
   end
 
-  def offer_contract startup, offer # offer is float
-
+  def offer_contract startup, type, offer # offer is float
+    FundingRound.new(startup, self, type, offer.to_f)
   end
 
   def funding_rounds
-
+    FundingRound.all.select{ |fr| fr.investor == self }
   end
 
   def portfolio
-
+    funding_rounds.map{ |fr| fr.startup }.uniq
   end
 
   def biggest_investment
-
+    funding_rounds.sort_by{ |fr| fr.investment }[-1]
   end
 
   def invested domain
-
+    funding_rounds.select{ |fr| fr.startup.domain == domain }
+      .map{ |fr| fr.investment }
+      .sum
   end
     
 end
--- tools/console.rb
+++ tools/console.rb
@@ -9,6 +9,22 @@ end
 
 webfries = Startup.new('Web Fries', 'Mister Fryman', 'www.frymanonline.com')
 troy = Startup.new('Get Help with Deanna', 'Counselor Troy', 'www.helpwiththecounselor.starfleet.com')
+warpdrive = Startup.new('Go Boldly', "Zephram Cochrane", 'lightspeecpheonix.com')
+peace = Startup.new('Peace in our Time', 'Ghandi', 'peace.gov')
+kvetch = Startup.new('Kvetch Bucket', 'Bubbe', 'kvetchbucket.com')
+
+richyrich = VentureCapitalist.new('Richy Rich', 1500000000)
+sirpendleton = VentureCapitalist.new('Sir Pendleton', 4250000000)
+roy = VentureCapitalist.new('Roy Royce', 843000000)
+liz = VentureCapitalist.new('Elizabeth McBeth', 45000000)
+
+richyrich.offer_contract(peace,'np',230000)
+richyrich.offer_contract(webfries, 'type a', 250000)
+richyrich.offer_contract(warpdrive, 'type 1', 800000000)
+richyrich.offer_contract(warpdrive, 'type b', 100000000)
+
+liz.offer_contract(warpdrive, 'type b', 100000000)
+roy.offer_contract(warpdrive, 'type b', 82000000)
 
 
 binding.pry
